39532a159e70e9e091d5a8da0e3a6c13bbe73798
Nefelijm <sistemasnik20@gmail.com>
2018-03-05 04:30:05 -0500

    Crando segundo componente biografia

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './Header.css';
+import './Main.css';
 import Header from './Header';
+import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Header/>, document.getElementById('header'));
+ReactDOM.render(<Main/>, document.getElementById('main'));
 registerServiceWorker();
