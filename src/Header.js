db0e2fc405b00745fd88bdc41ef0abff6d7562b3
Nefelijm <sistemasnik20@gmail.com>
2018-03-05 03:50:52 -0500

    Crando primer componente Haeder

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header-title">
+        <h1>Grace Hopper</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
